cbb06568e48abeab70a222bf1f35a2de2cf791f3
Quilamir <eyal@colu.co>
2018-07-05 17:15:59 +0300

    start

---
+++ index.js
@@ -0,0 +1,44 @@
+const port = 3000
+const express = require('express')
+const path = require('path')
+const EventEmitter = require('events')
+const randomNumberEmitter = new EventEmitter()
+var app = express()
+app.use(express.static(path.join(__dirname, 'static')))
+
+const bodyParser = require('body-parser')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+var config = {
+  defaultEventRate: 1000
+}
+
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+io.on('connection', function (socket) {
+  socket.on('start', function () {
+    console.log('starting')
+    randomNumberEmitter.on('newNumber', function (number) {
+      console.log('newNumber', number)
+      socket.emit('newNumber', number)
+    })
+  })
+})
+
+server.listen(port, function () {
+  // start creating events
+  setInterval(function () {
+    for (let i = 0; i < config.defaultEventRate; i++) {
+      let number = Math.floor(Math.random() * Math.floor(20))
+      console.log('number', number)
+      randomNumberEmitter.emit('newNumber', number)
+    }
+  }, 1000)
+
+  console.log('server listetning on port', port)
+})
