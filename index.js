d2e2cdd922544c79cafef7cdb9cc79c5892e7a8d
Quilamir <eyal@colu.co>
2018-07-05 18:17:50 +0300

    tweaked event rates

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 var config = {
-  defaultEventRate: 1000
+  defaultEventRate: 100
 }
 
 const server = require('http').createServer(app)
@@ -24,7 +24,7 @@ io.on('connection', function (socket) {
   socket.on('start', function () {
     console.log('starting')
     randomNumberEmitter.on('newNumber', function (number) {
-      console.log('newNumber', number)
+      // console.log('newNumber', number)
       socket.emit('newNumber', number)
     })
   })
@@ -35,10 +35,9 @@ server.listen(port, function () {
   setInterval(function () {
     for (let i = 0; i < config.defaultEventRate; i++) {
       let number = Math.floor(Math.random() * Math.floor(20))
-      console.log('number', number)
-      randomNumberEmitter.emit('newNumber', number)
+      randomNumberEmitter.emit('newNumber', {number: number, time: Date.now()})
     }
-  }, 1000)
+  }, 200)
 
   console.log('server listetning on port', port)
 })
