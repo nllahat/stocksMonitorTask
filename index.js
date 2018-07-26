b6d95ae15c3434a0842df6e600ff63ff98b74e84
Neta Li Lahat <netali@rndrobin.com>
2018-07-26 12:06:19 +0300

    First

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-const port = 3000
-const express = require('express')
-const path = require('path')
-const EventEmitter = require('events')
-const randomNumberEmitter = new EventEmitter()
-var app = express()
-app.use(express.static(path.join(__dirname, 'static')))
+const port = 3000;
+const express = require('express');
+const path = require('path');
+const EventEmitter = require('events');
+const randomNumberEmitter = new EventEmitter();
+var app = express();
+app.use(express.static(path.join(__dirname, 'static')));
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 var config = {
-  defaultEventRate: 100
-}
+    defaultEventRate: 100
+};
 
-const server = require('http').createServer(app)
-const io = require('socket.io')(server)
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
 io.on('connection', function (socket) {
-  socket.on('start', function () {
-    console.log('starting')
-    randomNumberEmitter.on('newNumber', function (number) {
-      // console.log('newNumber', number)
-      socket.emit('newNumber', number)
+    socket.on('start', function () {
+        console.log('starting');
+        randomNumberEmitter.on('newNumber', function (number) {
+            console.log('newNumber', number);
+            socket.emit('newNumber', number);
+        })
     })
-  })
-})
+});
 
 server.listen(port, function () {
-  // start creating events
-  setInterval(function () {
-    for (let i = 0; i < config.defaultEventRate; i++) {
-      let number = Math.floor(Math.random() * Math.floor(20))
-      randomNumberEmitter.emit('newNumber', {number: number, time: Date.now()})
-    }
-  }, 200)
+    // start creating events
+    setInterval(function () {
+        for (let i = 0; i < config.defaultEventRate; i++) {
+            let number = Math.floor(Math.random() * Math.floor(20));
+            randomNumberEmitter.emit('newNumber', { number: number, time: Date.now() });
+        }
+    }, 200);
 
-  console.log('server listetning on port', port)
-})
+    console.log('server listetning on port', port)
+});
